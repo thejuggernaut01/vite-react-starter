e8aaaf1d4e60488586bb7169beb01c67f2d58223
Ayoola Adewale <111049444+thejuggernaut01@users.noreply.github.com>
2025-04-01 22:36:16 +0100

    Update environment.ts

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -6,6 +6,6 @@ interface IENVIRONMENT {
 
 export const ENVIRONMENTS: IENVIRONMENT = {
   BASE_URL: {
-    API: import.meta.env.VITE_WELLAT_API_URL as string,
+    API: import.meta.env.VITE_BASE_API_URL as string,
   },
 };
